225cf2bef9db68f7b8d70b3268ba06963c7b739d
Nicholas <crazedglitch@yahoo.com>
2023-09-30 12:51:30 -0500

    models.py boiler plate added

---
+++ server/models.py
@@ -0,0 +1,52 @@
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+from flask import abort 
+
+from config import db, Bcrypt
+
+bcrypt = Bcrypt()
+
+# Models go here!
+
+
+
+
+# Model template!
+# class NameOfClass( db.Model ):
+#   __tablename__ = 'name of class plural'
+
+#   id = db.Column( db.Integer, primary_key = True )
+#   created_at = db.Column( db.DateTime, server_default=db.func.now() )
+#   updated_at = db.Column( db.DateTime, server_onupdate=db.func.now() )
+
+#   table_column_id = db.Column( db.Integer, db.ForeignKey( 'table.id' ) )
+
+#   def __repr__( self ):
+#       return f"{{ ModelName { self.id } }}"
+
+#   validation_errors = []
+
+#   def get_validation_errors( self ):
+#       return list( set( self.validation_errors ) )
+
+#   @classmethod
+#   def clear_validation_errors( cls ):
+#       cls.validation_errors = []
+
+# Password stuff for user model:
+#   @hybrid_property
+#   def password_hash( self ):
+#       return self._password_hash
+
+#   @password_hash.setter
+#   def password_hash( self, password ):
+#       if ***password validation goes in here!*** :
+#           password_hash = bcrypt.generate_password_hash( password.encode( 'utf-8' ) )
+#           self._password_hash = password_hash.decode( 'utf-8' )
+#       else :
+#           self.validation_errors.append( "Password validation error goes here!" )
+
+#   def authenticate( self, password ):
+#       return bcrypt.check_password_hash( self._password_hash, password.encode( 'utf-8' ) )
